3c39e378ec880fc8ab6bd5be0e68d5c9cec4f603
kuu7life <kuu7life@gmail.com>
2022-04-28 15:54:17 +0800

    Initial Commit

---
+++ SHIBCOIN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SHIBCOIN
+//
+//  Created by Kutman Amangeldiev on 2022/4/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
